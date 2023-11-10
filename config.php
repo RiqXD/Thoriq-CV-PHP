5e653c8331ed9f1600a1eb1462cdc12879a5d723
RiqXD <137005652+RiqXD@users.noreply.github.com>
2023-11-11 01:02:17 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$db = 'cv';
+$user = 'Thoriq2';
+$pwd = 'tesaja123';
+
+$conn = mysqli_connect($host, $user, $pwd, $db); // true | false
+
+if (!$conn) {
+  die('Gagal terhubung ke database'. mysqli_connect_error());
+}
